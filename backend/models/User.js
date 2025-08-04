498e14e15ce458f1bbfbd9e41d2ef55a1982adc9
Inkrypth <edwincali2002@gmail.com>
2025-08-04 20:06:52 +0800

    User Model Final Draft -variables, required, and type -password hashing function

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,5 +4,14 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
    name: { type: String, required: true },
    email: { type: String, required: true, unique: true },
-   password: {}
+   password: { type:String, required: true }
 });
+
+// Password Hashing
+userSchema.pre('save', async function (next) {
+   if (!this.isModified('password')) return next();
+   this.password = await bcrypt.hash(this.password, 10);
+   next();
+});
+
+module.exports = mongoose.model("User", userSchema);
