07ffe861ca4cad4569ed100024744473f8a83a48
Inkrypth <edwincali2002@gmail.com>
2025-08-04 21:29:14 +0800

    Login -route -controller
    Email lowercase consistency

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,9 +5,12 @@ const createToken = (userID) => {
     return jwt.sign({ id: userID }, process.env.JWT_SECRET, { expiresIn: '1d'});
 };
 
+// Signup function
 exports.signup = async (req, res) => {
     try {
-        const { name, email, password } = req.body;
+        const name = req.body.name;
+        const email = req.body.email.toLowerCase();
+        const password = req.body.password;
 
         // Prevent email duplication
         const existing = await User.findOne({ email });
@@ -29,5 +32,39 @@ exports.signup = async (req, res) => {
         });
     } catch (err) {
         res.status(500).json({ message: 'Signup failed', error: err.message });
+    }
+};
+
+// Login function
+exports.login = async (req, res) => {
+    try {
+        const email = req.body.email.toLowerCase();
+        const password = req.body.password;
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ 
+            message: 'User does not exist' 
+        });
+
+        // Compare password
+        const isMatch = await require('bcrypt').compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: 'Invalid email or password' });
+
+        // Generate token
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1d'});
+
+        res.status(200).json({
+            message: 'Login successful',
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            },
+            token
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Login failed', error: err.message });
     }
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../controllers/authController');
+const { signup, login } = require('../controllers/authController');
 const requireAuth = require('../middleware/authMiddleware');
 
 // Public routes
 router.post('/signup', signup);
+router.post('/login', login);
 
 
 
@@ -13,6 +14,9 @@ router.get('/me', requireAuth, (req, res) => {
     res.json({ message: 'You are authenticated', userID: req.user });
 });
 
+// add profile
+// add posts
+// add reactions
 
 
 module.exports = router;
