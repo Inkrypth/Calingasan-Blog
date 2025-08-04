a4161313e540a43a72eec779bcff3c89789ee0d9
Inkrypth <edwincali2002@gmail.com>
2025-08-04 20:55:21 +0800

    Signup function and API -controller -password hashing -JWT token -postman test of signup route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,3 +5,29 @@ const createToken = (userID) => {
     return jwt.sign({ id: userID }, process.env.JWT_SECRET, { expiresIn: '1d'});
 };
 
+exports.signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // Prevent email duplication
+        const existing = await User.findOne({ email });
+        if (existing) return res.status(400).json({ 
+            message: 'Email already registered' 
+        });
+
+        const user = await User.create({ 
+            name, 
+            email, 
+            password 
+        });
+
+        const token = createToken(user._id);
+        res.status(201).json({ 
+            message: 'User created successfully',
+            user: { id: user._id, name: user.name, email: user.email }, 
+            token
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Signup failed', error: err.message });
+    }
+};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { signup } = require('../controllers/authController');
 
+// Test Route
 router.get('/test', (req, res) => {
     res.json({ message: 'Auth route working!' });
 });
+
+// Signup route
+router.post('/signup', signup);
 
 module.exports = router;
